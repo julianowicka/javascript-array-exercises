8289ad4778e628c6b49dbcfec1e8d33887e30627
Julia Nowicka <julia.nowickaa@gmail.com>
2023-05-04 18:14:30 +0200

    Add the task description

---
+++ 02.js
@@ -0,0 +1,6 @@
+/*2. Write a JavaScript function to clone an array. Go to the editor
+Test Data :
+console.log(array_Clone([1, 2, 4, 0]));
+console.log(array_Clone([1, 2, [4, 0]]));
+[1, 2, 4, 0]
+[1, 2, [4, 0]]*/
