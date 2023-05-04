0cbd121d4fd26ecb73eeda216f424fb62500bbeb
Julia Nowicka <julia.nowickaa@gmail.com>
2023-05-04 16:32:09 +0200

    Add the task solution

--- 01.js
+++ 01.js
@@ -4,3 +4,8 @@ console.log(is_array('w3resource'));
 console.log(is_array([1, 2, 4, 0]));
 false
 true*/
+function is_array(input) {
+    return Array.isArray(input);
+}
+console.log(is_array('w3resource'));
+console.log(is_array([1, 2, 4, 0]));
