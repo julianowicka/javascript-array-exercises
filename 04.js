5783676768687ca2244ce561d47fbbd037e7a881
Julia Nowicka <julia.nowickaa@gmail.com>
2023-05-05 16:46:12 +0200

    Add the task description

---
+++ 04.js
@@ -0,0 +1,10 @@
+/*Write a JavaScript function to get the last element of an array. Passing the parameter 'n' will return the last 'n' elements of the array. Go to the editor
+Test Data :
+console.log(last([7, 9, 0, -2]));
+console.log(last([7, 9, 0, -2],3));
+console.log(last([7, 9, 0, -2],6));
+Expected Output :
+-2
+[9, 0, -2]
+[7, 9, 0, -2]*/
+
