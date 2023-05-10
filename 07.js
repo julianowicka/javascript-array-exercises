c3cf9a762375c0b0dffcadec59a800c2e6352c8b
Julia Nowicka <julia.nowickaa@gmail.com>
2023-05-10 22:45:48 +0200

    Add the task solution

--- 07.js
+++ 07.js
@@ -1,3 +1,11 @@
 /*Write a JavaScript program to sort the items of an array. Go to the editor
 Sample array : var arr1 = [ -3, 8, 7, 6, 5, -4, 3, 2, 1 ];
 Sample Output : -4,-3,1,2,3,5,6,7,8*/
+
+const arr1 = [-3, 8, 7, 6, 5, -4, 3, 2, 1];
+
+arr1.sort(function(a, b) {
+    return a - b;
+});
+
+console.log(arr1);
